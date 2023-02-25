f5a85be33c496da9ef6a7b14fcb18d71139e8eed
Henok Ayenew <117871961+henacodes@users.noreply.github.com>
2023-02-25 04:01:07 +0400

    Update api.js

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:8000";
+const URL = "https://fb-clone-backend.onrender.com";
 
 export const createPost = (postData, token) =>
   axios.post(`${URL}/posts/create`, postData, {
